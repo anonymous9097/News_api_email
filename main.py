8dc1d0b0d6c52451d18bf4a62ee4078a0f80c75d
divyangkhare <divyang.khare01@gmail.com>
2024-02-19 18:11:08 +0530

    added links, and filtered out news accordingly.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api = "8588afec044e47d59e67401c98431ecd"
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-01-18&"
-       "sortBy=publishedAt"
-       "&apiKey=8588afec044e47d59e67401c98431ecd")
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "from=2024-01-18&"
+       "sortBy=publishedAt&"
+       "apiKey=8588afec044e47d59e67401c98431ecd&"
+       "language=en")
 
 # Make Requests
 request = requests.get(url)
@@ -13,11 +17,12 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
-body = ""
-for article in content["articles"]:
+body = "Subject: Daily News!\n\n"
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + str(article["title"]) + "\n" + str(article["description"]) + 2*"\n"
+        body = (body + str(article["title"]) + "\n"
+                + str(article["description"]) + "\n"
+                + str(article["url"]) + 2 * "\n")
 
 body = body.encode("utf-8")
-send_email(message=body)
-
+send_email(body)
